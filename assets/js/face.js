1f3bb5ef05d2398b21348c3f3d0d2e6ee9e292ea
Huan Hock Lim <hlim307@gatech.edu>
2020-02-26 08:37:52 +0800

    2020 Update!

---
+++ assets/js/face.js
@@ -0,0 +1,16 @@
+// face.js
+var img = document.querySelector('.image.fill img');
+
+new BreathingHalftone( img, 
+  {
+  dotSize: 1/250,
+  dotSizeThreshold: 0.01,
+  initVelocity: 0.7,
+  oscPeriod: 2,
+  channels: [ 'lum' ],
+  friction: 0.25,
+  hoverDiameter: 0.4,
+  hoverForce: 0.004,
+  activeDiameter: -0.4,
+  activeForce: 0.004}
+);
